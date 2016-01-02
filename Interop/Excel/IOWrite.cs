7b8c2174aaf335d01e9c6d71b3e2b7919cb626ed
Vasil Hitrov <vhitrov@hotmail.com>
2016-01-02 13:42:36 +0200

    Видео 04 и 05 - Добавяне на клас за експорт към Excel и Дефиниране на библиотеки на Excel

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using InteropExcel=Microsoft.Office.Interop.Excel;
 
 namespace Excel
 {
 	public class IOWrite
 	{
 		private DataStruct _data;
+		private InteropExcel.Application excel;
 
 		public IOWrite (DataStruct data)
 		{
+		}
+
+		private bool exportTable ()
+		{
+			try
+			{
+				//Междинни проверки
+
+				return true;
+			}catch{
+			}
+			return false;
+
+		}
+
+		private void addRow (DataRow _row)
+		{
+			try{
+				
+
+			}catch{
+
+			}
+		}
+
+		private void runFile()
+		{
+			try{
+
+			}catch{
+			}
+		}
+
+		private string getPath()
+		{
+			return System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "Table1.xlsx");
 		}
 	}
 }
