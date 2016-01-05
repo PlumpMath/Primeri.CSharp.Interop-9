54de7d3c2b5f0d366f5b5fa2cfd7428d58e4f7dc
Vasil Hitrov <vhitrov@hotmail.com>
2016-01-05 17:49:12 +0200

    Модул 09, Видео 09 - Запис на данни в създадения sheet

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using InteropExcel=Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 namespace Excel
 {
@@ -10,6 +11,7 @@ namespace Excel
 
 		public IOWrite (DataStruct data)
 		{
+			_data = data;
 		}
 
 		public bool exportTable ()
@@ -30,6 +32,14 @@ namespace Excel
 
 				//Попълване на таблицата
 
+				int i=1;
+				addRow (new DataRow ("Първо име", "Фамилия", "Години"), i++);
+
+				foreach (DataRow row in _data.table)
+				{
+					addRow(row, i++);
+				}
+
 				//Запаметяване и затваряне
 				workbook.SaveCopyAs(getPath());
 
@@ -38,6 +48,17 @@ namespace Excel
 				workbook.Close();
 				excel.Quit();
 
+				//Освобождаване на паметта от Excel
+				if (workbook != null) Marshal.ReleaseComObject (workbook);
+				if (sheet    != null) Marshal.ReleaseComObject (sheet);
+				if (excel    != null) Marshal.ReleaseComObject (excel);
+
+				workbook = null;
+				sheet    = null;
+				excel    = null;
+
+				GC.Collect ();
+
 				return true;
 			}catch{
 			}
@@ -45,10 +66,20 @@ namespace Excel
 
 		}
 
-		public void addRow (DataRow _row)
+		public void addRow (DataRow _datarow, int _indexRow)
 		{
 			try{
 				
+				InteropExcel.Range range;
+
+				range=excel.Range["A" + _indexRow.ToString(), "A" + _indexRow.ToString()];
+				range.Value2 = _datarow.firsName;
+
+				range=excel.Range["B" + _indexRow.ToString(), "B" + _indexRow.ToString()];
+				range.Value2 = _datarow.lastName;
+
+				range=excel.Range["C" + _indexRow.ToString(), "C" + _indexRow.ToString()];
+				range.Value2 = _datarow.age;
 
 			}catch{
 
